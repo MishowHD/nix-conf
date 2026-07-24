070a67459d6066f4b8269c7b1e7c6e10dbdf3f35
Giacomo Di Clerico <giacomodiclerico@gmail.com>
2026-07-24 12:14:00 +0200

    feat(server): add user ssh public key to root authorizedKeys

--- modules/nixos/server/user.nix
+++ modules/nixos/server/user.nix
@@ -5,8 +5,7 @@
   users.users.root = {
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
-      # Paste your public SSH key here (e.g. cat ~/.ssh/id_ed25519.pub)
-      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI..."
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZZII1621J/ZJA4p0F5DDZIMeLJ7cg/M1WZynetxpP8 mishow@misharch"
     ];
   };
 }
